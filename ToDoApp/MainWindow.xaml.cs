6c34140afc938954fc99da789b59899c0b6d5d8b
gabor <gdombovari58@gmail.com>
2025-07-12 14:45:08 +0200

    Added basic task creation logic with task properties

--- ToDoApp/MainWindow.xaml.cs
+++ ToDoApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,6 +10,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Xml.Linq;
 
 namespace ToDoApp
 {
@@ -17,13 +19,20 @@ namespace ToDoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<taskItem> taskActive { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
             DispatcherTimer LiveTime = new DispatcherTimer();
             LiveTime.Interval = TimeSpan.FromSeconds(1);
             LiveTime.Tick += timer_Tick;
             LiveTime.Start();
+
+            taskActive = new ObservableCollection<taskItem>();
+            todolist.ItemsSource = taskActive;
+
         }
         void timer_Tick(object sender, EventArgs e)
         {
@@ -32,6 +41,14 @@ namespace ToDoApp
 
         private void buttonAddTask_Click(object sender, RoutedEventArgs e)
         {
+            var newTask = new taskItem { isNew=true, isImportantChecked=false, isImportantColor=false, contentTask=inputTaskBox.Text };
+
+            if (!string.IsNullOrWhiteSpace(inputTaskBox.Text))
+            {
+                taskActive.Add(newTask);
+                inputTaskBox.Clear();
+            }
+
 
         }
 
---
+++ ToDoApp/taskFormat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ToDoApp
+{
+    public class taskItem : INotifyPropertyChanged
+    {
+
+        public bool isNew { get; set; }
+        public bool isImportantChecked { get; set; }
+        public bool isImportantColor { get; set; }
+        public int timeRemain { get; set; }
+        public string? contentTask {  get; set; }
+        public Brush? importantColor { get; set; }
+
+        public string? timeOfCreation { get; set; }
+        public string? timeLeft { get; set; }
+        public bool? finishedTask { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
