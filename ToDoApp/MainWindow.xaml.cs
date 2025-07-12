2107f9d393632c9d8aeff402a589c8550b4d3992
gabor <gdombovari58@gmail.com>
2025-07-12 13:37:34 +0200

    added time and date

--- ToDoApp/MainWindow.xaml.cs
+++ ToDoApp/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ToDoApp
 {
@@ -19,6 +20,14 @@ namespace ToDoApp
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer LiveTime = new DispatcherTimer();
+            LiveTime.Interval = TimeSpan.FromSeconds(1);
+            LiveTime.Tick += timer_Tick;
+            LiveTime.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            currentTimeDate.Content = DateTime.Now.ToString("HH:mm:ss | dd/MM/yyyy");
         }
 
         private void buttonAddTask_Click(object sender, RoutedEventArgs e)
